d0f84efe470372fdfd3c7fa46be8724e79ad16c2
Odumosu Dipo <ironkyo@gmail.com>
2022-03-17 21:23:18 +0100

    feat: update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,12 @@
 async function main() {
   const MeffyCoin = await ethers.getContractFactory("MeffyCoin");
+  const FunkyMonkey = await ethers.getContractFactory("FunkyMonkey");
 
   // Start deployment, returning a promise that resolves to a contract object
-  const meffyCoin = await MeffyCoin.deploy();   
-  console.log("Contract deployed to address:", meffyCoin.address);
+  const meffyCoin = await MeffyCoin.deploy();
+  const funkyMonkey = await FunkyMonkey.deploy();
+  console.log("MeffyCoin deployed to address:", meffyCoin.address);
+  console.log("FunkyMonkey deployed to address:", funkyMonkey.address);
 }
 
 main()
